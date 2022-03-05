dde640d52f5192e16901a39daf7448dbb97175ef
finbazartech <finbazartech@gmail.com>
2022-03-05 22:23:09 +0600

    cursor

--- js/cursor.js
+++ js/cursor.js
@@ -2,32 +2,31 @@ let cursor = document.getElementById('cursor')
 let header = document.getElementById('header')
 let mailme = document.getElementById('mailme')
 let number = 1
+let oldX = 0
 document.onmousemove = function (e) {
-    console.log('screen.width', screen.width)
     cursor.style.left = (e.pageX - 25) + "px"
     cursor.style.top = (e.pageY - 25) + "px"
     cursor.style.height = "50px"
     cursor.style.width = "50px"
     cursor.style.display = "block"
-
+    console.log('e.pageX', e.pageX);
+    if(oldX<e.pageX) cursor.style.transform= "scaleX(1)"
+    else cursor.style.transform= "scaleX(-1)"
     if (e.pageY <= 60) {
-        cursor.style.display ="none"
+        // cursor.style.display ="none"
         cursor.style.left = (e.pageX-10) + "px"
         cursor.style.top = (e.pageY-20) + "px"
-        cursor.style.backgroundImage = "linear-gradient(yellow, #c4a300)";
-        background: ;
+        // cursor.style.backgroundImage = "linear-gradient(yellow, #c4a300)";
         cursor.style.transitionDuration = "0.5s";
         cursor.style.height = "20px"
         cursor.style.width = "20px"
-        cursor.style.border = "0px"
         cursor.style.transitionTimingFunctio = "ease-out";
     } else {
         // cursor.style.left = (e.pageX-60) + "px"
         cursor.style.top = (e.pageY-50) + "px"
-        cursor.style.backgroundImage = "none";
+        // cursor.style.backgroundImage = "none";
         cursor.style.transitionDuration = "0.5s";
         cursor.style.transitionTimingFunctio = "ease-in";
-        cursor.style.border= "3px solid #e7c82f";
     }
-
+    oldX = e.pageX
 }
